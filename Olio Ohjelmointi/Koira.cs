aa49096817f7f35e08a33fcb2f47a06835456f54
Emmi Muurinen <emmi.muurinen@student.careeria.fi>
2023-01-09 12:21:28 +0200

    olioharjoittelua

---
+++ Olio Ohjelmointi/Koira.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Ohjelmointi
+{
+    internal class Koira
+    {
+        public string Nimi;
+        public string Rotu;
+        public int Ikä;
+        
+        public void HaeTiedot()
+        {
+            Console.WriteLine("Koiran nimi on" + Nimi);
+            Console.WriteLine("Koiran ikä on" + Ikä);
+            Console.WriteLine("Koiran rotu on" + Rotu);
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine(Nimi + " woof woof ");
+        }
+    }
+}
--- Olio Ohjelmointi/Program.cs
+++ Olio Ohjelmointi/Program.cs
@@ -10,6 +10,22 @@ namespace Olio_Ohjelmointi
     {
         static void Main(string[] args)
         {
+            Koira koira1= new Koira(); // Luodaan uusi koira olio/instanssi
+            koira1.Nimi = " Rekku ";
+            koira1.Ikä = 2;
+            koira1.Rotu = " Mopsi ";
+
+            koira1.HaeTiedot();
+
+            Koira koira2 = new Koira();
+            koira2.Nimi = " Ali ";
+            koira2.Ikä = 5;
+            koira2.Rotu = " Bulldog ";
+
+            koira2.HaeTiedot();
+
+            koira1.Hauku();
+            koira2.Hauku();
         }
     }
 }
